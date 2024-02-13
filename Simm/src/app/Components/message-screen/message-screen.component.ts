893cbbae49082dcf68d15f3c9d49e72eb7a0ee1a
Grayson <GMcClead@sportsbusinessjournal.com>
2024-02-13 11:22:00 -0600

    implemented message service

--- Simm/src/app/Components/message-screen/message-screen.component.ts
+++ Simm/src/app/Components/message-screen/message-screen.component.ts
@@ -1,10 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MessageService } from '../../Services/message.service';
 
 @Component({
   selector: 'app-message-screen',
   templateUrl: './message-screen.component.html',
   styleUrl: './message-screen.component.sass'
 })
-export class MessageScreenComponent {
-  
+export class MessageScreenComponent implements OnInit{
+  messages: any[] = []
+  constructor(private messageService: MessageService){
+    
+  }
+  ngOnInit(): void {
+    this.messageService.messages$.subscribe((array) => {
+      this.messages = array 
+    })
+  }
 }
--- Simm/src/app/Services/message.service.ts
+++ Simm/src/app/Services/message.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  private messagesSubject = new BehaviorSubject<string[]>([])
 
+  messages$ = this.messagesSubject.asObservable()
+
+  updatedMessages(message: string){
+    const currentMessage = this.messagesSubject.getValue()
+    const updatedMessage = [...currentMessage, message]
+    this.messagesSubject.next(updatedMessage)
+  }
+  
   constructor() { }
 }
